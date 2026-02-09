64286958ef20a651ae9d671a9849667f986f5cf7
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2026-02-09 09:03:04 -0300

    Add project files.

---
+++ ReportAnalizerWASM/ReportAnalizerWASM.Client/Models/TransaccionMP.cs
@@ -0,0 +1,14 @@
+﻿namespace ReportAnalizerWASM.Client.Models
+{
+    public class TransaccionMP
+    {
+        public string DATE { get; set; }
+        public string DESCRIPTION { get; set; }
+        public decimal GROSS_AMOUNT { get; set; }
+        public decimal MP_FEE_AMOUNT { get; set; }
+        public decimal TAXES_AMOUNT { get; set; }
+        public decimal NET_CREDIT_AMOUNT { get; set; }
+        public decimal NET_DEBIT_AMOUNT { get; set; }
+        public string PAYMENT_METHOD { get; set; }
+    }
+}
